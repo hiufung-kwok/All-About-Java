36a16d1b20284bb145b363b72264263bfd622e16
Hiu Kwok <hiufkwok@gmail.com>
2018-08-27 22:35:48 +0800

    Example added: Null obj pattern

---
+++ src/designPattern/NullObjectPattern.java
@@ -0,0 +1,13 @@
+package designPattern;
+
+
+/*
+* Ref: https://dzone.com/articles/null-object-pattern-in-java?utm_medium=feed&utm_source=feedpress.me&utm_campaign=Feed:%20dzone%2Fjava
+*
+* The point here is handle null as normal, which null shouldn't be treat as abnormal. Instead it's just nothing.
+*
+* Java8 Optional something really close and handy to use.
+*
+* */
+public class NullObjectPattern {
+}
