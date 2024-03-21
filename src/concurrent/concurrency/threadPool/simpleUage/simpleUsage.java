b1e572ff7e49274933d7047a59d64a16939f145e
HiuFung Kwok <hiufkwok@gmail.com>
2024-03-21 10:26:07 +0800

    Update package

---
+++ src/concurrent/concurrency/threadPool/simpleUage/simpleUsage.java
@@ -0,0 +1,42 @@
+package concurrent.concurrency.threadPool.simpleUage;
+//Ref: http://tutorials.jenkov.com/java-concurrency/threadlocal.html
+public class simpleUsage {
+
+	public static class NewTask implements Runnable {
+		
+		private ThreadLocal<Integer> t_local = new ThreadLocal<Integer> ();
+
+		@Override
+		public void run() {
+			
+			t_local.set( (int) (Math.random() * 100D));
+			
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				
+			}
+			
+			System.out.println("Value: " + t_local.get());
+			
+		}
+		
+		
+		public static void main(String[] args) throws InterruptedException {
+			NewTask shareInstance = new NewTask();
+
+	        Thread t1 = new Thread(shareInstance);
+	        Thread t2 = new Thread(shareInstance);
+	        
+	        t1.start();
+	        t2.start();
+
+	        t1.join();
+	        t2.join(); 
+		}
+		
+	}
+	
+	
+	
+}
