8e3be05f6ed25d8e751524f4f33a3b3828546c93
HiuFung Kwok <hiufkwok@gmail.com>
2024-04-15 16:08:08 +0800

    Update sync q

---
+++ src/collection/queue/blockingQueue/SyncBlockingQueueTest.java
@@ -0,0 +1,26 @@
+package collection.queue.blockingQueue;
+
+import java.time.Duration;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+
+/**
+ * Similar to blocking queue with capacity == 1.
+ */
+public class SyncBlockingQueueTest {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        BlockingQueue bq = new SynchronousQueue();
+
+        SampleProducer pd = new SampleProducer(bq);
+        SampleConsumer cs = new SampleConsumer(bq);
+
+        new Thread(pd).start();
+        new Thread(cs).start();
+
+        Thread.sleep(Duration.ofMinutes(3));
+
+    }
+}
