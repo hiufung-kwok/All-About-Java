71fc767d72ee073e3cc9cfed935eda0bb071206a
HiuFung Kwok <hiufung.kwok.852@gmail.com>
2024-06-01 22:12:37 -0700

    String update

--- src/other/StringPlayground.java
+++ src/other/StringPlayground.java
@@ -1,5 +1,7 @@
 package other;
 
+import java.util.stream.IntStream;
+
 public class StringPlayground {
 
     public static void main(String[] args) {
@@ -61,6 +63,62 @@ public class StringPlayground {
         // Ideally, should allocate one SB per thread, to have both performance and thread-safe.
         StringBuilder sb = new StringBuilder("Hello");
         StringBuffer sbf = new StringBuffer("Hello");
+
+
+        boolean result = "12345".matches("\\\\\\\\\\\\\\\\d+");
+        System.out.println("One or more numbers: " + result);
+
+        // Reverse
+        String versed = new StringBuilder(hello).reverse().toString();
+        System.out.println(versed);
+
+
+        // Compare ignore cass
+        boolean sameIgnoreCase = "hello".equalsIgnoreCase("HELLO");
+        System.out.println(sameIgnoreCase);
+
+        // getBytes, encodes the string into a sequence of bytes using the platform's
+        // default charset.
+        byte[] bytes = "Hello".getBytes();
+
+
+        // StartWith, with index
+        boolean startWithBool = "HelloWorld".startsWith("World", 5);
+
+        boolean regionalMatch = "HelloWorld".regionMatches(0, "Hello", 0 , 5);
+
+        // Replace all
+        System.out.println("HelloWorld".replaceAll("l", "x"));
+
+        // Replace the first one.
+        "HelloWorld".replaceFirst("l", "L");
+
+        // String to intStream, return an IntStream of Unicode code points from the string.
+        IntStream helloStream = hello.codePoints();
+
+
+        String joinned = String.join("Hello", "world");
+
+        // Composite a string from partial Char[ ]
+        char[] chars ={'a', 'b', 'c', 'd', 'e'};
+        String charArr = new String(chars, 1, 3);
+        System.out.println(charArr);
+
+        // Chars( ) return a IntStream instead of char []
+        IntStream charStream = hello.chars();
+
+        // End with
+        boolean endsWith = "Hello".endsWith("lo");
+
+
+
+
+
+
+
+
+
+
 
 
 
